dd408cc1b970ca661464c06be8b933194986c6ba
Nicotrz <nicolas.trz@gmail.com>
2019-03-05 14:56:59 +0100

    Adding the code to add picture from the photo library with imagePicker

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 
+    enum PickerChoose {
+        case firstSquare
+        case secondSquare
+        case thirdSquare
+        case fourthSquare
+        case firstRectangle
+        case secondRectangle
+    }
+    
+    var pickerChoose: PickerChoose = .firstSquare
+    
     @IBOutlet weak var myView: PictureView!
     @IBOutlet weak var firstRectangleView: UIView!
+    @IBOutlet weak var firstRectangleImage: UIImageView!
     @IBOutlet weak var secondRectangleView: UIView!
+    @IBOutlet weak var secondRectangleImage: UIImageView!
     @IBOutlet weak var firstSquareView: UIView!
+    @IBOutlet weak var firstSquareImage: UIImageView!
     @IBOutlet weak var secondSquareView: UIView!
+    @IBOutlet weak var secondSquareImage: UIImageView!
     @IBOutlet weak var thirdSquareView: UIView!
+    @IBOutlet weak var thirdSquareImage: UIImageView!
     @IBOutlet weak var fourthSquareView: UIView!
+    @IBOutlet weak var fourthSquareImage: UIImageView!
     @IBOutlet weak var selectedSquareFirst: UIImageView!
     @IBOutlet weak var selectedSquareSecond: UIImageView!
     @IBOutlet weak var selectedSquareThird: UIImageView!
+    @IBOutlet weak var swipeLabel: UILabel!
+    @IBOutlet weak var arrowLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -28,6 +47,83 @@ class ViewController: UIViewController {
         myView.setPictureView(firstSquareView: firstSquareView, secondSquareView: secondSquareView, thirdSquareView: thirdSquareView, fourthSquareView: fourthSquareView, firstRectangleView: firstRectangleView, secondRectangleView: secondRectangleView, selectedSquareFirst: selectedSquareFirst, selectedSquareSecond: selectedSquareSecond, selectedSquareThird: selectedSquareThird)
     }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            swipeLabel.text = "Swipe right to share"
+            arrowLabel.text = ">"
+        } else {
+            swipeLabel.text = "Swipe up to share"
+            arrowLabel.text = "^"
+        }
+    }
+
+    @IBAction func addPictureToFirstRectangle(_ sender: Any) {
+        callPicker(withPicker: .firstRectangle)
+    }
+    
+    @IBAction func addPictureToSecondRectangle(_ sender: Any) {
+        callPicker(withPicker: .secondRectangle)
+    }
+    
+    @IBAction func addPictureToFirstSquare(_ sender: Any) {
+        callPicker(withPicker: .firstSquare)
+    }
+    
+    @IBAction func addPictureToSecondSquare(_ sender: Any) {
+        callPicker(withPicker: .secondSquare)
+    }
+    
+    @IBAction func addPictureToThirdSquare(_ sender: Any) {
+        callPicker(withPicker: .thirdSquare)
+    }
+    
+    @IBAction func addPictureToFourthSquare(_ sender: Any) {
+        callPicker(withPicker: .fourthSquare)
+    }
+    
+    
+    func callPicker(withPicker picker: PickerChoose) {
+        pickerChoose = picker
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        switch pickerChoose {
+        case .firstRectangle:
+            firstRectangleImage.image = selectedImage
+        case .secondRectangle:
+            secondRectangleImage.image = selectedImage
+        case .firstSquare:
+            firstSquareImage.image = selectedImage
+        case .secondSquare:
+            secondSquareImage.image = selectedImage
+        case .thirdSquare:
+            thirdSquareImage.image = selectedImage
+        case .fourthSquare:
+            fourthSquareImage.image = selectedImage
+        }        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+
+    //handle the cancel
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+    
     @IBAction func switchToFirstDisplay(_ sender: Any) {
         myView.state = .firstDisplay
     }
